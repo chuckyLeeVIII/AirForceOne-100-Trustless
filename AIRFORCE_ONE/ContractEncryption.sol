69c7b70b82e3d34b53c36c1f9a29ecbfa53c3324
ChuckY_LeeVIII aka 2Mr.Nixon2 <153417564+chuckyLeeVIII@users.noreply.github.com>
2025-01-25 12:30:11 -0800

    Update ContractEncryption.sol

--- AIRFORCE_ONE/ContractEncryption.sol
+++ AIRFORCE_ONE/ContractEncryption.sol
@@ -60,3 +60,58 @@ interface IERC20 {
 #################################################################################################################################################################################################################
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #################################################################################################################################################################################################################
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract ContractEncryption {
+    bytes32 private encryptionKey;
+    address public tokenAddress;
+    uint256 public requiredBalance;
+
+    mapping(address => bool) public authorizedContracts;
+    mapping(bytes32 => bytes) private encryptedCode;
+
+    event ContractEncrypted(bytes32 indexed contractHash);
+    event ContractAuthorized(address indexed contractAddress);
+
+    constructor(address _tokenAddress, uint256 _requiredBalance, bytes32 _encryptionKey) {
+        tokenAddress = _tokenAddress;
+        requiredBalance = _requiredBalance;
+        encryptionKey = _encryptionKey;
+    }
+
+    function encryptContract(bytes memory contractCode) external returns (bytes32) {
+        require(IERC20(tokenAddress).balanceOf(msg.sender) >= requiredBalance, "Insufficient token balance");
+
+        bytes32 contractHash = keccak256(abi.encodePacked(contractCode, block.timestamp));
+        bytes memory encrypted = _encrypt(contractCode);
+        encryptedCode[contractHash] = encrypted;
+
+        emit ContractEncrypted(contractHash);
+        return contractHash;
+    }
+
+    function authorizeContract(address contractAddress, bytes32 contractHash) external {
+        require(IERC20(tokenAddress).balanceOf(msg.sender) >= requiredBalance, "Insufficient token balance");
+        require(encryptedCode[contractHash].length > 0, "Contract not found");
+
+        authorizedContracts[contractAddress] = true;
+        emit ContractAuthorized(contractAddress);
+    }
+
+    function _encrypt(bytes memory data) internal view returns (bytes memory) {
+        bytes memory result = new bytes(data.length);
+        for(uint i = 0; i < data.length; i++) {
+            result[i] = data[i] ^ bytes1(uint8(uint256(encryptionKey) >> (i % 32 * 8)));
+        }
+        return result;
+    }
+
+    function _decrypt(bytes memory encrypted) internal view returns (bytes memory) {
+        return _encrypt(encrypted); // XOR encryption is reversible
+    }
+}
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+}
